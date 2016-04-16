29a81de586fbb331dc5e92023e99dda104f291c4
Miroslaw Syzdek <miroslaw.syzdek@gmail.com>
2016-04-16 18:04:52 +0200

    Added rng validation

--- rng.sol
+++ rng.sol
@@ -1,15 +1,13 @@
 contract rng {
 	
-	function getRandom(uint numOfBlocks, uint searchSpace) returns (uint randomNum) {
-		bytes32 startingBlock = block.blockhash(0);
-		
+	function getRandom(bytes32 startingBlock, uint numOfBlocks, uint searchSpace) returns (uint randomNum) {
+		int blocks = validateNumOfBlocks(numOfBlocks);
 		uint value = 0;
-		
 		
 		for (int i = 256; i >= 0; i--) {
 			if (startingBlock == block.blockhash(uint(i))) {
 				value = uint(startingBlock);
-				for (int j=1; j < int(numOfBlocks); j++){
+				for (int j=1; j < blocks; j++){
 					value = value & uint(block.blockhash(uint(i+j)));
 				}
 				return value % searchSpace;
@@ -19,6 +17,11 @@ contract rng {
 	}
 	
 	function validateNumOfBlocks(uint numOfBlocks) private returns (int) {
-		
+		if (numOfBlocks > 256 ){
+			throw;
+		}
+		return int(numOfBlocks);
 	}
+	
+	
 }
